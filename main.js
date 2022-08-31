b6b9dcc0531e02a855206e5de8bacba14cc65392
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-01 02:01:21 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// const loadDrinks = async() => {
+//     const url = await `https://www.thecocktaildb.com/api/json/v1/1/search.php?s`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data.drinks);
+// }
+
+// // 
+
+// loadDrinks()
