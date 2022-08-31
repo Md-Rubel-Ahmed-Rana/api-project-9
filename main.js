99ff13d54bee43dbb1e4382eecb92ba253ca1d37
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-01 02:02:56 +0600

    Data loading complete

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// const loadDrinks = async() => {
-//     const url = await `https://www.thecocktaildb.com/api/json/v1/1/search.php?s`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     console.log(data.drinks);
-// }
+const loadDrinks = async() => {
+    const url = await `https://www.thecocktaildb.com/api/json/v1/1/search.php?s`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.drinks);
+}
 
-// // 
-
-// loadDrinks()
+loadDrinks()
